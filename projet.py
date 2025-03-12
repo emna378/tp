cca2fb369d49d77972599ff6fa61286688e6e646
=emna <emnajlassi234@gmail.com>
2025-03-12 23:35:37 +0100

    projet.py commited if else

--- projet.py
+++ projet.py
@@ -1,5 +1,8 @@
 data=[1,2,3]
-print('la somme est :' ,sum[data])
-print('le minest :' , min[data])
-print('le max est : ', max[data])
+if data :
 
+    print('la somme est :' ,sum[data])
+    print('le minest :' , min[data])
+    print('le max est : ', max[data])
+else: 
+    print('dossier vide')
